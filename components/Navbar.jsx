d223bc8a84fcc77124c6cf123e0c0c3905ac16a1
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2023-04-23 22:49:14 -0700

    tailwind configuration implemented

---
+++ components/Navbar.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 
+      2xl:px-16'>
+        <Image 
+        src="/../public/assets/navLogo.png"
+         alt="/" 
+         width='80' 
+         height='50' />
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
